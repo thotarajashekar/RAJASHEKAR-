bc3f38edb9473721f7aa8d0088bf48f984b167aa
thotarajashekar <thotarajashekar773@gmail.com>
2025-03-12 12:27:47 +0530

    Add files via upload
    MYSQL

---
+++ raja.sql
@@ -0,0 +1,34 @@
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    CustomerName VARCHAR(255),
+    ContactName VARCHAR(255),
+    Address VARCHAR(255),
+    City VARCHAR(255),
+    PostalCode VARCHAR(20),
+    Country VARCHAR(255)
+);
+
+INSERT INTO customers (CustomerID, CustomerName, ContactName, Address, City, PostalCode, Country) VALUES
+(1, 'Alfreds Futterkiste', 'Maria Anders', 'Obere Str. 57', 'Berlin', '12209', 'Germany'),
+(2, 'Ana Trujillo Emparedados y helados', 'Ana Trujillo', 'Avda. de la Constitución 2222', 'México D.F.', '05021', 'Mexico'),
+(3, 'Antonio Moreno Taquería', 'Antonio Moreno', 'Mataderos 2312', 'México D.F.', '05023', 'Mexico'),
+(4, 'Around the Horn', 'Thomas Hardy', '120 Hanover Sq.', 'London', 'WA1 1DP', 'UK'),
+(5, 'Berglunds snabbköp', 'Christina Berglund', 'Berguvsvägen 8', 'Luleå', 'S-958 22', 'Sweden');
+
+SELECT * FROM customers;
+create database CUSTOMERS; 
+use  customers;
+SELECT * FROM Customers
+WHERE Country='Mexico';
+SELECT * FROM customers
+
+WHERE CustomerID = 2;
+SELECT * FROM Customers
+WHERE Country='Germany' AND City='Berlin';
+SELECT * FROM Customers
+ORDER BY Country;
+
+  SELECT * FROM Customers
+ORDER BY ADRESS;
+
+
